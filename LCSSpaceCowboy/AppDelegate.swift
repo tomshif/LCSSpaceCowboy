e6d549a1dbf71ff4b4d2be9a891859365a71997c
Tom Shiflet <tom.shiflet@mylcs.com>
2021-01-22 14:40:17 -0600

    Initial Commit

---
+++ LCSSpaceCowboy/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  LCSSpaceCowboy
+//
+//  Created by Tom Shiflet on 1/22/21.
+//  Copyright © 2021 LCS Game Design. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
