ff3cd52b0d383ed24101f888aba0a90702f20e5c
Drennan Ryan <drennan.ryan21@mylcs.com>
2021-01-26 14:57:26 -0600

    Initial setup

--- LCSSpaceCowboy/New Group/PlayerClass.swift
+++ LCSSpaceCowboy/New Group/PlayerClass.swift
@@ -10,18 +10,87 @@ import SpriteKit
 
 class PlayerClass
 {
-    // Variables
-    
+    var shieldCurrent:CGFloat = 100
+    var shieldCurrentMax:CGFloat = 100
+    var shieldBase:CGFloat = 100
+    var laserDamageCurrent:CGFloat = 100
+    var laserDamageCurrentMax:CGFloat = 100
+    var laserDamageBase:CGFloat = 100
+    var laserSpeedCurrent:CGFloat = 100
+    var laserSpeedCurrentMax:CGFloat = 100
+    var laserSpeedBase:CGFloat = 100
+    var playerSpeedCurrent:CGFloat = 100
+    var playerSpeedBase:CGFloat = 100
+    var hullCurrent:CGFloat = 100
+    var hullCurrentMax:CGFloat = 100
+    var hullBase:CGFloat = 100
+    var powerUpTimeCurrent:CGFloat = 100
+    var powerUpTimeBase:CGFloat = 100
+    var powerUpCooldownCurrent:CGFloat = 100
+    var powerUpCooldownBase:CGFloat = 100
+    var active1 = TalentClass()
+    var active2 = TalentClass()
+    var active3 = TalentClass()
+    //variables
     
     // Constants
     
-    
     // Optionals
-    
     
     init()
     {
         
-    } // init()
+    }// init()
     
+    func moveLeft ()
+    {
+        
+    }//moveLeft
+    
+    func moveRight ()
+    {
+        
+    }//moveRight
+
+    func moveUp ()
+    {
+        
+    }//moveUp
+    
+    func moveDown ()
+    {
+        
+    }//moveDown
+    
+    func fireLaser ()
+    {
+        
+    }//fireLaser
+    
+    func fireActive1 ()
+    {
+      
+        
+        
+    }//fireActive1
+    
+    func fireActive2 ()
+    {
+       
+       
+        
+    }//fireActive2
+    
+    func fireActive3 ()
+    {
+       
+       
+        
+    }//fireActive3
+    
+    func update ()
+    {
+    
+    }//update
+
 } // PlayerClass
