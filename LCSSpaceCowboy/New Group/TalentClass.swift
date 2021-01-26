83c4ed2ab7f45b113029749136bf9b074be7ba5b
Blake jasuta <Blake.Jasuta21@mylcs.com>
2021-01-26 14:55:12 -0600

    Initial talent class setup

--- LCSSpaceCowboy/New Group/TalentClass.swift
+++ LCSSpaceCowboy/New Group/TalentClass.swift
@@ -11,10 +11,53 @@ import SpriteKit
 class TalentClass
 {
     // Variables
+    var shieldInc:CGFloat = 0
+    var damageInc:CGFloat = 0
+    var hullInc:CGFloat = 0
+    var powerUpDuration: CGFloat = 0
+    var activeAbilityCooldown: CGFloat = 0
+    var shieldRecharge:CGFloat = 0
+    var laserCooldown:CGFloat = 0
+    var hullHeal:CGFloat = 0
+    var coolDown:Double = 0
+    var lastUsed = NSDate()
     
+    func pulseKnockback()
+    {
+        
+    }//knocks back enemies that are close
     
+    func laserBeam()
+    {
+        
+    }//fires a continuous laser
+    
+    func reflectShield()
+    {
+        
+    }//reflects enemy lasers
+    
+    func slowTime()
+    {
+        
+    }//slows down enemy movements
+    
+    func missileSwarm()
+    {
+        
+    }//fires a swarm of missiles that target enemies
+    
+    func shotgunLaser()
+    {
+        
+    }//wide spread laser
+    
+    func deployMine()
+    {
+        
+    }//deploys mines that explode on contact
     // Constants
-    
+
     
     // Optionals
     
@@ -23,5 +66,10 @@ class TalentClass
     {
         
     } // init()
+    
+    func update()
+    {
+        
+    }//Update
     
 } // TalentClass
