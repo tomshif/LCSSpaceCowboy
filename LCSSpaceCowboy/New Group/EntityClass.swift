55086c96f5f8fa0ce9bca83c45d4ea9c5f0a35bd
Cole Flemmons <cole.flemmons21@mylcs.com>
2021-01-28 14:32:21 -0600

    scene

--- LCSSpaceCowboy/New Group/EntityClass.swift
+++ LCSSpaceCowboy/New Group/EntityClass.swift
@@ -10,13 +10,30 @@ import SpriteKit
 
 class EntityClass
 {
+    var enemy1 = SKSpriteNode(imageNamed:"enemyBlack1")
+   // var enemy2 = SKSpriteNode(imageNamed:"enemyBlack2")
+   // var enemy3 = SKSpriteNode(imageNamed:"enemyBlack3")
+    
+    
     // Variables
-    
-    
+    var health:CGFloat = 0
+    var shieldHealth:CGFloat = 0
+    var isAlive:Bool = true
     // Constants
-    
+    var speed:Int = 0
     
     // Optionals
+    var cactus:Bool = false
+    var boss:Bool = false
+    
+   
+    var theScene:GameScene?
+    init(scene:GameScene){
+        theScene = scene
+        theScene!.hudAnchor.addChild(enemy1)
+        enemy1.position.x = 400
+        
+    }//init scene:gamescene
     
     
     init()
