53271c3410a49217a9f5a5b721710a6d4a2e3733
Cole Flemmons <cole.flemmons21@mylcs.com>
2021-02-01 13:58:28 -0600

    ddd

--- LCSSpaceCowboy/New Group/EntityClass.swift
+++ LCSSpaceCowboy/New Group/EntityClass.swift
@@ -11,12 +11,6 @@ import SpriteKit
 class EntityClass
 {
     
-    func update(){
-        
-        
-        
-    }//update()
-    
     
     var sprite = SKSpriteNode(imageNamed:"enemyBlack1")
    // var enemy2 = SKSpriteNode(imageNamed:"enemyBlack2")
@@ -27,20 +21,26 @@ class EntityClass
     var health:CGFloat = 0
     var shieldHealth:CGFloat = 0
     var isAlive:Bool = true
+    var enemyMovementType:CGFloat = random(min: 0, max:1)
     // Constants
     var speed:Int = 0
     
     // Optionals
     var cactus:Bool = false
     var boss:Bool = false
-    
-   
+    var theGame:GameClass?
     var theScene:GameScene?
-    init(scene:GameScene){
+    
+
+    
+    
+    
+    init(scene:GameScene, game:GameClass){
         theScene = scene
         theScene!.gameAnchor.addChild(sprite)
         sprite.position.x = 400
-        
+        sprite.zRotation = CGFloat.pi
+        //135
     }//init scene:gamescene
     
     
@@ -48,5 +48,51 @@ class EntityClass
     {
         
     } // init()
+   // func fireLaser(){
+       // let laser=SKSpriteNode(imageNamed: "laserGreen11")
+       // laser.position = sprite.position
+       // laser.zRotation = sprite.zRotation
+       // laser.zPosition = 10
+        //laserDaddy.addChild(laser)
+               
+       // let tempx = cos(laser.zRotation)*1000+laser.position.x
+      //  let tempy = sin(laser.zRotation)*1000+laser.position.y
+     //   let   laserSeq = SKAction.sequence([SKAction.move(to: CGPoint(x: tempx, y: tempy), duration: 0.25), SKAction.removeFromParent()])
+      //  laser.run(laserSeq)
+      
+           
+           
+       //}//fired
+    
+    
+    func movement(){
+        
+        sprite.position.y += 5
+        
+        
+        
+        
+       // if(enemyMovementType == 0){
+        if(sprite.position.y < -600){
+            sprite.position.y += 5
+            
+        }//if far down go up
+        if(sprite.position.y > 600){
+            sprite.position.y -= 5
+            
+        }//if far up go down
+      //  }//if type is 0
+        
+        
+        }//movement function
+   
+    
+    
+    func update(){
+    movement()
+    }//update
+    
+    
+    
     
 } // EntityClass
