0b2e6d590c0e846f0bdc79dbebde53fa6d607c8b
Drennan Ryan <drennan.ryan21@mylcs.com>
2021-02-01 14:14:38 -0600

    Fixed player movement and beginning ability to fire laser

--- LCSSpaceCowboy/New Group/PlayerClass.swift
+++ LCSSpaceCowboy/New Group/PlayerClass.swift
@@ -11,31 +11,46 @@ import SpriteKit
 class PlayerClass
 {
     var sprite = SKSpriteNode (imageNamed: "enemyBlue1")
+    var laser = SKSpriteNode (imageNamed: "laserGreen05")
+    //objects
     var verticalSpeed:CGFloat = 16
     var horizontalSpeed:CGFloat = 10
     var edgeSpeed:CGFloat = 0
+    //speeds
     var shieldCurrent:CGFloat = 100
     var shieldCurrentMax:CGFloat = 100
     var shieldBase:CGFloat = 100
+    //shields
     var laserDamageCurrent:CGFloat = 100
     var laserDamageCurrentMax:CGFloat = 100
     var laserDamageBase:CGFloat = 100
+    //laser damage
     var laserSpeedCurrent:CGFloat = 100
     var laserSpeedCurrentMax:CGFloat = 100
     var laserSpeedBase:CGFloat = 100
+    //laser speed
+    var lastLaserFire=NSDate()
+    let LASER_COOLDOWN:Double=0.1
+    //laser creation
     var playerSpeedCurrent:CGFloat = 100
     var playerSpeedBase:CGFloat = 100
+    //player speed
     var hullCurrent:CGFloat = 100
     var hullCurrentMax:CGFloat = 100
     var hullBase:CGFloat = 100
+    //hull integrity
     var powerUpTimeCurrent:CGFloat = 100
     var powerUpTimeBase:CGFloat = 100
+    //power up time
     var powerUpCooldownCurrent:CGFloat = 100
     var powerUpCooldownBase:CGFloat = 100
+    //power up cooldown
     var active1 = TalentClass()
     var active2 = TalentClass()
     var active3 = TalentClass()
+    //active talents
     var theScene:GameScene?
+    //game scene
     //variables
     
     // Constants
@@ -106,12 +121,16 @@ class PlayerClass
             verticalSpeed = edgeSpeed
             sprite.position.y = -theScene!.size.height/2 + sprite.size.height/2 + 4
         }
-        
     }//moveDown
     
     func fireLaser ()
     {
-        
+        if -lastLaserFire.timeIntervalSinceNow > LASER_COOLDOWN
+        {
+            laser.position = sprite.position
+            laser.zPosition = sprite.zPosition - 1
+            theScene!.gameAnchor.addChild(laser)
+        }
     }//fireLaser
     
     func fireActive1 ()
