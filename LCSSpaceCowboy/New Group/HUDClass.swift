d1de4d3b75940e63db48f26ebda0606189129b2b
Beau <robert.cooper22@mylcs.com>
2021-02-03 14:23:35 -0600

    almost done :)

--- LCSSpaceCowboy/New Group/HUDClass.swift
+++ LCSSpaceCowboy/New Group/HUDClass.swift
@@ -26,6 +26,7 @@ class HUDClass
     
     var theScene: GameScene?
     
+    
     // Variables
     
     
@@ -48,14 +49,38 @@ class HUDClass
     {
         theScene=scene
         
+    //theScene!.size.width
+    
+    theScene!.hudAnchor.addChild(powerUp)
+        powerUp.position.x = theScene!.size.width*0.45
+        powerUp.position.y = -theScene!.size.height*0.40
         
-        theScene!.hudAnchor.addChild(hudSheild)
+    theScene!.hudAnchor.addChild(hudSheild)
+        hudSheild.position.x = -theScene!.size.width*0.48
+        hudSheild.position.y = -theScene!.size.height*0.40
         
         theScene!.hudAnchor.addChild(special1)
+        special1.position.x = -theScene!.size.width*0.45
+        special1.position.y = -theScene!.size.height*0.20
+
         
-        theScene!.hudAnchor.addChild(special2)
+    theScene!.hudAnchor.addChild(special2)
+        special2.position.x = -theScene!.size.width*0.45
+        special2.position.y = -theScene!.size.height*0.30
+
         
-        theScene!.hudAnchor.addChild(special3)
+    theScene!.hudAnchor.addChild(special3)
+        special3.position.x = -theScene!.size.width*0.45
+        special3.position.y = -theScene!.size.height*0.10
+        
+    theScene!.hudAnchor.addChild(score)
+        score.text = "Score: "
+        score.fontSize = 25
+        score.fontColor = NSColor.white
+        score.position.x = theScene!.size.width*0.2
+        score.position.y = theScene!.size.height*0.44
+        
+
         
     }
     // init(gamescene)
