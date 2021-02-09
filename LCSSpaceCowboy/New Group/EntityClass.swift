1c7ddc4877339965914fa012ac71c925da39eddf
Cole Flemmons <cole.flemmons21@mylcs.com>
2021-02-09 13:48:32 -0600

    d

--- LCSSpaceCowboy/New Group/EntityClass.swift
+++ LCSSpaceCowboy/New Group/EntityClass.swift
@@ -13,6 +13,7 @@ class EntityClass
     
     
     var sprite = SKSpriteNode(imageNamed:"enemyBlack1")
+    var laser = SKSpriteNode(imageNamed:"laserRed03")
    // var enemy2 = SKSpriteNode(imageNamed:"enemyBlack2")
    // var enemy3 = SKSpriteNode(imageNamed:"enemyBlack3")
     
@@ -21,25 +22,25 @@ class EntityClass
     var health:CGFloat = 0
     var shieldHealth:CGFloat = 0
     var isAlive:Bool = true
-    var enemyMovementType:CGFloat = random(min: 0, max:1)
+    var enemyMovementType:Int = Int(random(min: 1, max:2.99999))
     // Constants
-    var speed:CGFloat = 5
-    
+    var speedY:CGFloat = 5
+    var speedX:CGFloat = 10
     // Optionals
     var cactus:Bool = false
     var boss:Bool = false
     var theGame:GameClass?
     var theScene:GameScene?
     
-
-    
-    
     
     init(scene:GameScene, game:GameClass){
         theScene = scene
         theScene!.gameAnchor.addChild(sprite)
+        theScene!.gameAnchor.addChild(laser)
         sprite.position.x = 400
+        sprite.position.y = random(min: -400, max: 400)
         sprite.zRotation = CGFloat.pi
+
         //135
     }//init scene:gamescene
     
@@ -66,19 +67,32 @@ class EntityClass
     
     
     func movement(){
-        
+      
         
         // All movement should be modified by the theScene!.gameAnchor.speed in order to handle the time slow
-        sprite.position.y += speed*theScene!.gameAnchor.speed
-        if(sprite.position.y > 600){
-            speed = -5
-        }//if positon is higher than 600
-        if(sprite.position.y < -600){
-               speed = 5
-           }//if positon is higher than 600
-           
-        }//movement function
-   
+    sprite.position.y += speedY*theScene!.gameAnchor.speed
+    if(sprite.position.y > 600){
+        speedY = -5
+            }//if positon is higher than 600
+    if(sprite.position.y < -600){
+        speedY = 5
+            }//if positon is higher than 600
+        if(sprite.position.y < -100){
+            speedY = 0
+            sprite.position.x -= speedX*theScene!.gameAnchor.speed
+            
+        }
+        if(sprite.position.x < -800){
+            sprite.position.x = 800
+            if(sprite.position.x == 400){
+                speedX = 0
+                speedY = 5
+                sprite.position.y += speedY*theScene!.gameAnchor.speed
+            }//back to normal
+        }
+    
+    
+    }//movement function
     
     
     func update(){
